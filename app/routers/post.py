bc407a884cf4137006c3c2714ed3f4a38508fb25
Pradip Thapa <thepradipthapa.404@gmail.com>
2025-06-30 21:38:31 +0545

    feat: assign owner_id to posts on creation

--- app/routers/post.py
+++ app/routers/post.py
@@ -29,7 +29,7 @@ def create_posts(post: schemas.PostCreate, db: session = Depends(get_db), curren
     # new_post = cursor.fetchone()
     # conn.commit()
 
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
 
     db.add(new_post)
     db.commit()
